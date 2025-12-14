581be35cf76ba0563922ff116e2366365adcfb7c
Michal Wasiak <michal.wasiak@hotmail.com>
2025-12-14 20:36:27 +1000

    Task 01 done

--- 01/app.js
+++ 01/app.js
@@ -4,14 +4,28 @@ import { createRoot } from 'react-dom/client';
 const root = createRoot(document.querySelector('#root'));
 
 class App extends React.Component {
-    state = {
-        counter: 0,
+    constructor(props) {
+        super(props);
+        this.state = { counter: 0 }
+        console.log('constructor');
     }
     render() {
-        console.log('render');
-
-        return <h1>{ this.state.counter }</h1>
+        console.log('render')
+        return <h1>{this.state.counter}</h1>
+    }
+    componentDidMount() {
+        console.log('componentDidMount');
+        this.id = setInterval(() => {
+            const { counter } = this.state;
+            this.setState({ counter: counter + 1 })
+        }, 5000);
+    }
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+        clearInterval(this.id)
     }
 }
-
-root.render(<App/>);
+root.render(<App />);
