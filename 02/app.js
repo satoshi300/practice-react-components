549b004cb51fe096f35d5796700bd817d63a281e
Michal Wasiak <michal.wasiak@hotmail.com>
2025-12-14 20:54:04 +1000

    Task 02 done

--- 02/app.js
+++ 02/app.js
@@ -7,9 +7,15 @@ class Counter extends React.Component {
     state = {
         amount: 0,
     }
-    
+    clickHandler = e => {
+        const { amount } = this.state;
+        this.setState({
+            amount: amount + 1
+        })
+        console.log(e.target.innerText)
+    }
     render() {
-        return <button>click me ({ this.state.amount })</button>
+        return <button onClick={this.clickHandler}>click me ({this.state.amount})</button>
     }
 }
 
