83109ef50fdd733fed53385bb01acb9769180650
Michal Wasiak <michal.wasiak@hotmail.com>
2025-12-16 11:32:32 +1000

    Task 03 done - missing separate <li> for each comment

--- 03/app.js
+++ 03/app.js
@@ -6,37 +6,60 @@ const root = createRoot(document.querySelector('#root'));
 class Article extends React.Component {
     state = {
         comments: [],
+        content: '',
     }
-    
+    contentChange = e => {
+        this.setState({
+            content: e.target.value,
+        })
+     }
+
+    submitHandler = e => {
+        e.preventDefault();
+        const { content } = this.state;
+        
+        this.addContent(`${content}`);
+        this.setState({
+            content: '',
+        })
+    }
+    addContent(content) {
+        this.setState({
+            comments: [this.state.comments, content],
+        })
+    }
     render() {
-        const {title, body} = this.props;
+        const { title, body } = this.props;
+        const { content } = this.state;
+
         return (
             <article>
-                <h1>{ title }</h1>
-                <p>{ body }</p>
+                <h1>{title}</h1>
+                <p>{body}</p>
                 <section>
-                    <form>
+                    <form onSubmit={this.submitHandler}>
                         <div>
                             <label>
-                                <textarea 
-                                    style={{ "minWidth": "300px", "minHeight": "120px" }} 
-                                    name="content" 
+                                <textarea
+                                    style={{ "minWidth": "300px", "minHeight": "120px" }}
+                                    name="content" value={content} onChange={this.contentChange}
                                 />
                             </label>
                         </div>
                         <div><input type="submit" value="dodaj komentarz" /></div>
                     </form>
                     <ul>
+                        <li>{this.state.comments}</li>
                         {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
                     </ul>
                 </section>
-            </article>
+            </article >
         )
     }
 }
 
 root.render(
-    <Article 
+    <Article
         title="Programowanie jest super!"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
     />
